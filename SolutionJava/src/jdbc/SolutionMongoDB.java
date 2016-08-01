ee0ee49dfd9ceaec5f42a5fea4f0854bdd864536
cenxui lin <b97201017@ntu.edu.tw>
2016-08-01 15:30:46 +0800

    initial

---
+++ SolutionJava/src/jdbc/SolutionMongoDB.java
@@ -0,0 +1,15 @@
+package jdbc;
+
+/**
+ * {@link http://mongodb.github.io/mongo-java-driver/3.3/driver/getting-started/quick-tour/}
+ * @author xenxui
+ * 2016/8/1
+ */
+public class SolutionMongoDB {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
