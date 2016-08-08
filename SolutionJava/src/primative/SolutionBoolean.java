12300ed03c5fa2d2881dd3e79781abf2b6707850
cenxui lin <b97201017@ntu.edu.tw>
2016-08-08 14:20:58 +0800

    add SolutionBoolean

---
+++ SolutionJava/src/primative/SolutionBoolean.java
@@ -0,0 +1,13 @@
+package primative;
+
+public class SolutionBoolean {
+
+	public static void main(String[] args) {
+		// boolean type can only put in true or false or other logical result.
+		boolean x = false;
+		boolean y = (10>5);
+//		boolean z = 1; this will make compile error
+
+	}
+
+}
